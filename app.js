be5e7171381dde88dc2b002a575f215f7003ada5
stark0085 <aayushvijay29@gmail.com>
2025-01-20 00:06:50 +0530

    apis for leaderboard for the game smash the cans

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(cors())
 dotenv.config()
 
 const PORT = 14452
+// const PORT = 4000
 
 var con = mysql.createConnection({
     host: process.env.HOST,
@@ -67,13 +68,11 @@ const checkExistingPlayer = (userdata) => {
     return new Promise((resolve, reject) => {
         con.query("SELECT * FROM leaderboard WHERE sfID = ?", [userdata.sfID], (err, result) => {
             if (err) {
-                res.status(500).json({ code: 2, message: "Error while fetching Player's details", error: err.message });
                 return reject(err)
             }
             if (result.length > 0 && result[0].points <= userdata.points) {
                 con.query("UPDATE leaderboard SET points = ? WHERE sfID = ?", [userdata.points, userdata.sfID], (err) => {
                     if (err) {
-                        // res.status(500).json({ code: 3, message: "Error while updating Player's points", error: err.message });
                         return reject(err)
                     }
                     resolve(true);
@@ -96,14 +95,14 @@ app.post('/player', async (req, res) => {
     try {
         const playerUpdated = await checkExistingPlayer(userdata);
         if (playerUpdated) {
-            return res.status(200).json({ code: 0, message: 'Player updated successfully' });
+            return res.status(200).json({ code: 0, message: 'Score updated successfully' });
         }
         const sql = `INSERT INTO leaderboard (user_name, sfID, points) VALUES (?, ?, ?)`;
         const values = [userdata.user_name, userdata.sfID, userdata.points];
 
         con.query(sql, values, (err, result) => {
             if (err) {
-                return res.status(500).json({ code: 4, message: "Player's score insertion failed ", error: err.message });
+                return res.status(500).json({ code: 4, message: "Player's score insertion failed"});
             }
             res.status(200).json({ code: 0, message: 'Player added successfully' });
         });
@@ -131,4 +130,86 @@ app.get('/player', (req, res) => {
     }
 });
 
-// module.exports = app
+// Smash the cans 
+
+app.get('/cans/players', (req, res) => {
+    try {
+        con.query("SELECT *, rank() OVER (ORDER BY points_m desc) AS players_rank FROM leaderboard_cans ORDER BY points_m DESC, user_name", function (err, players) {
+            if (err) {
+                return res.status(500).json({ code: 1, message: "Could not fetch the data", error: err.message })
+            }
+            else {
+                return res.status(200).json({ code: 0, message: "Players Fetched Successfully", players: players })
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ code: -1, message: 'Internal server error' });
+    }
+})
+
+const checkExistingPlayerCans = (userdata) => {
+    return new Promise((resolve, reject) => {
+        con.query("SELECT * FROM leaderboard_cans WHERE sfID = ?", [userdata.sfID], (err, result) => {
+            if (err) {
+                return reject(err)
+            }
+            if (result.length > 0 && result[0].points_m <= userdata.points_m) {
+                con.query("UPDATE leaderboard_cans SET points_m = ? WHERE sfID = ?", [userdata.points_m, userdata.sfID], (err) => {
+                    if (err) {
+                        return reject(err)
+                    }
+                    resolve(true);
+                });
+            }
+            else {
+                resolve(false)
+            }
+        });
+    });
+};
+
+app.post('/cans/player', async (req, res) => {
+    const userdata = req.body;
+
+    if (!userdata.user_name || !userdata.sfID || !userdata.points_m) {
+        return res.status(400).json({ message: 'Invalid input data' });
+    }
+
+    try {
+        const playerUpdated = await checkExistingPlayerCans(userdata);
+        if (playerUpdated) {
+            return res.status(200).json({ code: 0, message: 'Score updated successfully' });
+        }
+        const sql = `INSERT INTO leaderboard_cans (user_name, sfID, points_m) VALUES (?, ?, ?)`;
+        const values = [userdata.user_name, userdata.sfID, userdata.points_m];
+
+        con.query(sql, values, (err, result) => {
+            if (err) {
+                return res.status(500).json({ code: 4, message: "Player's score insertion failed"});
+            }
+            res.status(200).json({ code: 0, message: 'Player added successfully' });
+        });
+    } catch (error) {
+        res.status(500).json({ code: -1, message: 'Internal server error' });
+    }
+});
+
+
+app.get('/cans/player', (req, res) => {
+    const sfID = req.query.sfID;
+
+    if (!sfID) {
+        return res.status(400).json({ code: 1, message: "sfID is required" });
+    }
+
+    try {
+        con.query(`SELECT * FROM leaderboard_cans WHERE sfID = ?`, [sfID], function (err, player) {
+            if (err) {
+                return res.status(500).json({ code: 5, message: "Could not fetch the data", error: err.message });
+            }
+            res.status(200).json({ code: 0, message: "Player's details fetched successfully", player: player });
+        });
+    } catch (error) {
+        res.status(500).json({ code: -1, message: 'Internal server error' });
+    }
+});
